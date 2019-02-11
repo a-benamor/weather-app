798834d11bca5ee28c9f36d2826618ac4fdffd20
Adem Ben Amor <adem.benamor.ensi@gmail.com>
2019-02-11 20:14:59 +0100

    Fetch html element and add listener to click button event

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 /**
  * Created by adem on 09-Feb-19.
  */
-import {searchWeatherBtn} from 'htmlElements.js';
 
-console.log(searchWeatherBtn);
+import * as ELEMENTS from 'elements.js';
+
+ELEMENTS.ELEMENT_SHOW_WEATHER_BUTTON.addEventListener('click', searchWeather);
+
+function searchWeather(){
+    const CITY_NAME = ELEMENTS.ELEMENT_CITY_INPUT.value.trim();
+    if(CITY_NAME.length == 0){
+        return alert('You must enter a city name');
+    } else{
+        alert(CITY_NAME);
+    }
+
+}
