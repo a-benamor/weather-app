e633577a3980113151b3bae9cc5d9b89c8aa6219
Adem Ben Amor <adem.benamor.ensi@gmail.com>
2019-02-13 23:22:00 +0100

    Feature/weather app incrment max 3 (#4)
    * Fetch html element and add listener to click button event
    
    * Add calling get weather web service using promises
    
    * Add Fetch weather data and display it
    
    * Add heroku to resolve the CORS issue

--- app.js
+++ app.js
@@ -1,6 +1,48 @@
 /**
  * Created by adem on 09-Feb-19.
  */
-import {searchWeatherBtn} from 'htmlElements.js';
 
-console.log(searchWeatherBtn);
+import * as ELEMENTS from 'elements.js';
+import {HttpService} from 'httpService.js';
+import {OPEN_WEATHER_API_KEY} from 'openWeatherApiKey.js';
+import {WeatherData, WEATHER_PROXY_BEHAVIOR} from 'weather-data.js';
+
+
+
+ELEMENTS.ELEMENT_SHOW_WEATHER_BUTTON.addEventListener('click', searchWeather);
+
+function searchWeather(){
+    const CITY_NAME = ELEMENTS.ELEMENT_CITY_INPUT.value.trim();
+    if(CITY_NAME.length == 0){
+        ELEMENTS.ELEMENT_WEATHER_BOX.style.display = 'none';
+        ELEMENTS.ELEMENT_LOAD_BOX.style.display = 'none';
+        return alert('You must enter a city name');
+    } else{
+        console.log('the user city name value is : ' + CITY_NAME);
+        let herokuUrl = 'https://cors-anywhere.herokuapp.com/';
+        const URL = herokuUrl + 'http://samples.openweathermap.org/data/2.5/weather?q='+CITY_NAME+'&appid='+OPEN_WEATHER_API_KEY ;
+        console.log('The url is : ' + URL);
+        ELEMENTS.ELEMENT_WEATHER_BOX.style.display = 'none';
+        ELEMENTS.ELEMENT_LOAD_BOX.style.display = 'block';
+        HttpService.getData(URL).
+            then(responseData => {
+                console.log(responseData);
+                console.log(responseData.weather[0].description);
+                console.log(responseData.main.temp)
+                const WEATHER_OBJ = new WeatherData(CITY_NAME,responseData.weather[0].description);
+                const WEATHER_PROXY = new Proxy(WEATHER_OBJ, WEATHER_PROXY_BEHAVIOR);
+                WEATHER_PROXY.temperature = responseData.main.temp;
+                updateWeather(WEATHER_PROXY);
+
+        }).catch(error => {alert(error)});
+    }
+
+}
+
+function updateWeather(weatherProxy){
+    ELEMENTS.ELEMENT_WEATHER_CITY_NAME.textContent = weatherProxy.cityName;
+    ELEMENTS.ELEMENT_WEATHER_CITY_DESCRIPTION.textContent = weatherProxy.description;
+    ELEMENTS.ELEMENT_WEATHER_CITY_TEMPERATURE.textContent = weatherProxy.temperature;
+    ELEMENTS.ELEMENT_WEATHER_BOX.style.display = 'block';
+    ELEMENTS.ELEMENT_LOAD_BOX.style.display = 'none';
+}
