22df4824d4020f740ce5bdc33ee53f5c88f0deb9
adem-BA <adem.benamor.ensi@gmail.com>
2019-02-16 16:07:11 +0100

    Delete unused files

--- open-weather-map-api-key.js
+++ open-weather-map-api-key.js
@@ -2,5 +2,5 @@
  * Created by adem on 10-Feb-19.
  */
 
-// Specify your openWeatherMap API key
+// Specify your openWeatherMap API key once you have created an account
 export const OPEN_WEATHER_MAP_API_KEY = '';
